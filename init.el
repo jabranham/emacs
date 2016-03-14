8e340faf40531ac36f71a8aa01bf26a87287ef47
Alex Branham <branham@utexas.edu>
2016-03-14 15:46:34 -0500

    Sets up python mode

--- init.el
+++ init.el
@@ -25,6 +25,8 @@
 (add-to-list 'package-archives
              '("melpa" . "http://melpa.org/packages/"))
 (add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/") t)
+(add-to-list 'package-archives
+             '("elpy" . "http://jorgenschaefer.github.io/packages/"))
 (package-initialize)
 
 ;; Bootstrap `use-package'
