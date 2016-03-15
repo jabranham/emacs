1a07efce22b34dec8423b6c3cd89823aa9f6c3d0
Alex Branham <branham@utexas.edu>
2016-03-15 17:34:49 -0500

    Various minor changes

--- init.el
+++ init.el
@@ -5,18 +5,16 @@
   (unless (>= emacs-major-version minver)
     (error "Your Emacs is too old -- this config requires v%s or higher" minver)))
 
-;;
-;; This file loads Org-mode and then loads the rest of our Emacs initialization from Emacs lisp
-;; embedded in literate Org-mode files.
+;; This file loads Org-mode and then loads the rest of our Emacs
+;; initialization from Emacs lisp embedded in literate Org-mode files.
+;; It will load all files with a file name starting "load" and ending
+;; with "org"
 
-;; Load up Org Mode and (now included) Org Babel for elisp embedded in Org Mode files
 (setq dotfiles-dir (file-name-directory (or (buffer-file-name) load-file-name)))
-
 
 (setq inhibit-startup-message t ; disable start screen
       global-font-lock-mode t ; font lock (syntax highlighting) everywhere
       font-lock-maximum-decoration t) ; lots of color
-(add-to-list 'default-frame-alist '(fullscreen . maximized)) ; start maximized
 (setq gc-cons-threshold 20000000)
 
 ;; add MELPA, install use-package
@@ -29,7 +27,7 @@
              '("elpy" . "http://jorgenschaefer.github.io/packages/"))
 (package-initialize)
 
-;; Bootstrap `use-package'
+;; Bootstrap 'use-package' and 'org-mode
 (unless (package-installed-p 'use-package)
   (package-refresh-contents)
   (package-install 'use-package))
@@ -39,18 +37,17 @@
 
 (eval-when-compile
   (require 'use-package))
-(require 'diminish) ; if you use :diminish
-(require 'bind-key) ; if you use any :bind variant
-(require 'cl-lib) ; require common lisp expressions
-;(require 'cl) ; require common lisp
+(require 'diminish)
+(require 'bind-key)
+;(require 'cl-lib)
 
 ;; load up all literate org-mode files in this directory
 (require 'org)
-(require 'org-install)
-(require 'ob-tangle)
-(require 'ox)
+;(require 'org-install)
+;(require 'ob-tangle)
+;(require 'ox)
 
-(mapc #'org-babel-load-file (directory-files dotfiles-dir t "\\.org$"))
+(mapc #'org-babel-load-file (directory-files dotfiles-dir t "load.*\\.org$"))
 
 (setq gc-cons-threshold 800000)
 
