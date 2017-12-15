55b288dfa7b6c987a1d63ec6c5862808fea73bf5
Alex Branham <branham@utexas.edu>
2017-12-14 19:29:19 -0600

    org-agenda: setup home/work agendas

--- init.el
+++ init.el
@@ -1660,6 +1660,16 @@ See `org-agenda-todo' for more details."
             (tags "REFILE"
                   ((org-agenda-overriding-header "Tasks to Refile")
                    (org-tags-match-list-sublevels nil)))))
+          ("h" "Home Agenda"
+           ((agenda "" nil)
+            (tags "@home"
+                  ((org-agenda-overriding-header "Tasks to do at home")
+                   (org-tags-match-list-sublevels nil)))))
+          ("w" "Work Agenda"
+           ((agenda "" nil)
+            (tags "@work"
+                  ((org-agenda-overriding-header "Tasks to do at work")
+                   (org-tags-match-list-sublevels nil)))))
           ("d" "deadlines"
            ((agenda ""
                     ((org-agenda-entry-types '(:deadline))
