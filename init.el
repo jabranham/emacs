b8c1769e47112283a687abdbac06e4fe7b25c8dd
Alex Branham <branham@utexas.edu>
2018-03-07 14:48:55 -0600

    Update spacemacs-theme to eb4ed60
    - Don't underline parens

--- init.el
+++ init.el
@@ -2394,6 +2394,8 @@ there are no attachments."
 (use-package spacemacs-dark-theme
   ;; By default, emacs starts with a blindingly white theme.  Let's get rid
   ;; of that pronto.
+  :custom
+  (spacemacs-theme-underline-parens nil)
   :init
   (defvar my/theme 'spacemacs-dark
     "The theme I'm using.")
